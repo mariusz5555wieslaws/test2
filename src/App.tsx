65856f93efb162fe7455d85be59ef3c9279a5cdc
mariusz5555wieslaws <83507415+mariusz5555wieslaws@users.noreply.github.com>
2024-10-09 21:21:45 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -178,19 +178,19 @@ const App: React.FC = () => {
             <div className="px-4 mt-6 flex justify-between gap-2">
               <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
                 <div className="dot"></div>
-                <img src={dailyReward} alt="Daily Reward" className="mx-auto w-18 h-18" />
+                <img src={dailyReward} alt="Daily Reward" className="mx-auto w-16 h-16" />
                 <p className="text-[10px] text-center text-white mt-1">Daily reward</p>
                 <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyRewardTimeLeft}</p>
               </div>
               <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
                 <div className="dot"></div>
-                <img src={dailyCipher} alt="Daily Cipher" className="mx-auto w-18 h-18" />
+                <img src={dailyCipher} alt="Daily Cipher" className="mx-auto w-16 h-16" />
                 <p className="text-[10px] text-center text-white mt-1">Daily cipher</p>
                 <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyCipherTimeLeft}</p>
               </div>
               <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
                 <div className="dot"></div>
-                <img src={dailyCombo} alt="Daily Combo" className="mx-auto w-18 h-18" />
+                <img src={dailyCombo} alt="Daily Combo" className="mx-auto w-16 h-18" />
                 <p className="text-[10px] text-center text-white mt-1">Daily combo</p>
                 <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyComboTimeLeft}</p>
               </div>
