f54bf12fa1b72b7a69998c203e1a34fb6a8ac674
mariusz5555wieslaws <83507415+mariusz5555wieslaws@users.noreply.github.com>
2024-10-10 18:22:05 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -195,6 +195,29 @@ const App: React.FC = () => {
                 <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyComboTimeLeft}</p>
               </div>
             </div>
+            
+        <div className="flex-grow mt-4 bg-[#f3ba2f] rounded-t-[48px] relative top-glow z-0">
+          <div className="absolute top-[2px] left-0 right-0 bottom-0 bg-[#1d2025] rounded-t-[46px]">
+            <div className="px-4 mt-6 flex justify-between gap-2">
+              <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
+                <div className="dot"></div>
+                <img src={dailyReward} alt="Daily Reward" className="mx-auto w-17 h-17" />
+                <p className="text-[10px] text-center text-white mt-1">Daily reward</p>
+                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyRewardTimeLeft}</p>
+              </div>
+              <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
+                <div className="dot"></div>
+                <img src={dailyCipher} alt="Daily Cipher" className="mx-auto w-17 h-17" />
+                <p className="text-[10px] text-center text-white mt-1">Daily cipher</p>
+                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyCipherTimeLeft}</p>
+              </div>
+              <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
+                <div className="dot"></div>
+                <img src={dailyCombo} alt="Daily Combo" className="mx-auto w-17 h-17" />
+                <p className="text-[10px] text-center text-white mt-1">Daily combo</p>
+                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyComboTimeLeft}</p>
+              </div>
+            </div>
 
             <div className="px-4 mt-4 flex justify-center">
               <div className="px-4 py-2 flex items-center space-x-2">
